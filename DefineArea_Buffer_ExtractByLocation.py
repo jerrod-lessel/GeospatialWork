5518c5bcefd66bc8d3c1eb5a503b8b033ff95e94
Jerrod Lessel <jerrod.lessel@gmail.com>
2024-07-11 12:49:55 -0700

    Add files via upload

---
+++ DefineArea_Buffer_ExtractByLocation.py
@@ -0,0 +1,33 @@
+from qgis import processing
+
+# List of all agorithms within QGIS
+#for alg in QgsApplication.processingRegistry().algorithms():
+#    print(alg.id(),"->", alg.displayName())
+
+# The 'algorithmHelp' process basically gives you the readme for each algorithm
+#processing.algorithmHelp("qgis:selectbyattribute")
+
+buffer_distance = 0.1 #degrees
+my_gpkg = "/Users/jerrod/Documents/myStuff/Python_for_geospatial_data_analysis/natural_earth_vector.gpkg/packages/natural_earth_vector.gpkg"
+rivers = '{}|layername=ne_10m_rivers_lake_centerlines'.format(my_gpkg)
+places = '{}|layername=ne_10m_populated_places'.format(my_gpkg)
+expression = "name = 'Amazonas'"
+
+amazonas = processing.run("native:extractbyexpression",
+    {'INPUT':rivers,'EXPRESSION':expression,'OUTPUT':'memory:'}
+    )['OUTPUT']
+
+buffered_amazonas = processing.run("native:buffer",
+    {'INPUT':amazonas,'DISTANCE':buffer_distance,'SEGMENTS':5,'END_CAP_STYLE':0,
+    'JOIN_STYLE':0,'MITER_LIMIT':2,'DISSOLVE':False,'OUTPUT':'memory:'}
+    )['OUTPUT']
+
+places_along_amazonas = processing.run("native:extractbylocation",
+    {'INPUT':places,'PREDICATE':[0],'INTERSECT':buffered_amazonas,'OUTPUT':
+        'memory:'}
+    )['OUTPUT']
+
+QgsProject.instance().addMapLayer(places_along_amazonas)
+
+for feature in places_along_amazonas.getFeatures():
+    print(feature["name"])
